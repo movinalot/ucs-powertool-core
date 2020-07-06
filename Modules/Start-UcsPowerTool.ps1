c653bdc1dee5b2939843a9f2a615ca8d32bbae8e
John McDonough <movinalot@gmail.com>
2020-07-06 17:52:14 -0400

    Cisco UCS PowerTool 3.0.0.10

--- Modules/Start-UcsPowerTool.ps1
+++ Modules/Start-UcsPowerTool.ps1
@@ -5,26 +5,29 @@ Import-Module Cisco.IMC
 $version = Get-UcsPowerToolConfiguration
 write-host "          Welcome to Cisco UCS PowerTool Suite $($version.Version)"
 write-host ""
-write-host "Log in to a UCS Manager Domain:                                         " -NoNewLine
+write-host "Log in to UCS Manager:                                                  " -NoNewLine
 write-host "Connect-Ucs" -foregroundcolor yellow
-write-host "To generate PowerShell code from a running UCS Manager GUI:             " -NoNewLine
+write-host "To generate PowerShell code for UCS Manager use:                        " -NoNewLine
 write-host "ConvertTo-UcsCmdlet" -foregroundcolor yellow
-write-host "To show object metadata for all UCS PowerTool commands:                 " -NoNewLine
-write-host "Get-UcsCmdletMeta" -foregroundcolor yellow  
+write-host "To show object metadata for UCS PowerTool Cmdlets:                      " -NoNewLine
+write-host "Get-UcsCmdletMeta" -foregroundcolor yellow
 write-host ""
-write-host "Log in to an IMC of standalone UCS server:                              " -NoNewLine
+write-host "Log in to the IMC of standalone UCS server:                             " -NoNewLine
 write-host "Connect-Imc" -foregroundcolor yellow
-write-host "Generate PowerShell code from a running IMC of standalone UCS server:   " -NoNewLine
+write-host "To generate PowerShell code for an IMC of a standalone UCS server:      " -NoNewLine
 write-host "ConvertTo-ImcCmdlet" -foregroundcolor yellow
-write-host "Show object metadata for all IMC PowerTool cmdlets:                     " -NoNewLine
-write-host "Get-ImcCmdletMeta" -foregroundcolor yellow  
+write-host "To show object metadata for IMC PowerTool Cmdlets:                      " -NoNewLine
+write-host "Get-ImcCmdletMeta" -foregroundcolor yellow
 write-host ""
-write-host "Log in to a UCS Central server:                                         " -NoNewLine
+write-host "Log in to UCS Central:                                                  " -NoNewLine
 write-host "Connect-UcsCentral" -foregroundcolor yellow
-write-host "To generate PowerShell code from a running UCS Central GUI:             " -NoNewLine
+write-host "To generate PowerShell code for UCS Central use:                        " -NoNewLine
 write-host "ConvertTo-UcsCentralCmdlet" -foregroundcolor yellow
-write-host "To show object metadata for all UCS Central PowerTool commands:         " -NoNewLine
-write-host "Get-UcsCentralCmdletMeta" -foregroundcolor yellow  
-write-host "Once you've connected, display the details of active session(s):        " -NoNewLine
+write-host "To show object metadata for UCS Central PowerTool Cmdlets:              " -NoNewLine
+write-host "Get-UcsCentralCmdletMeta" -foregroundcolor yellow
+write-host ""
+write-host "To display the details of the UCS PowerTool Configuration:              " -NoNewLine
+write-host "Get-UcsPowerToolConfiguration" -foregroundcolor yellow
+write-host "To display details of UCS, IMC and UCS Central active session(s):       " -NoNewLine
 write-host "Get-UcsPSSession" -foregroundcolor yellow
 write-host ""
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # docker-ucs-powertool-core
-Docker Image for UCS PowerTool Core
+Docker Image for UCS PowerTool Core 3.0.0.10
 
-This docker image contains all required packages to run UCS PowerTool Core 3.0.0.10 for
+This docker image contains all required modules to run UCS PowerTool Core for
 
 - Cisco UCS Manager
 - Cisco UCS Central
 - Cisco IMC
 
-Using the container
+## Using the container
 
 The Container is CentOS 8 based and is running PowerShell Core 7.0.2  Upon launching the container all the Cisco UCS PowerShell Modules are Imported.
 
